7fa52dd27a6c8bcd5746b91bd2d829475101e8d3
Abu-muaz <plshireseid@gmail.com>
2025-10-03 19:03:03 +0300

    footer component added

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -6,7 +6,6 @@ import { FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-  
     <footer className={style["evangadi-footer"]}>
       <div className={style["footer-container"]}>
         <div className={style["footer-column"]}>
@@ -17,7 +16,7 @@ const Footer = () => {
             <a
               href="https://www.facebook.com/evangaditech"
               aria-label="Facebook"
-              target="_blank" 
+              target="_blank"
               rel="noopener noreferrer" // Added for security best practice
             >
               <FiFacebook />
@@ -44,7 +43,7 @@ const Footer = () => {
         {/* Column 2: Useful Links */}
         <div className={style["footer-column"]}>
           <h5 className={style["footer-heading"]}>Useful Link</h5>
-         
+
           <a href="/how-it-works" className={style["footer-link"]}>
             How it works
           </a>
@@ -67,12 +66,11 @@ const Footer = () => {
         </div>
 
         {/* Column 3: Contact Info */}
-        <div className={style["footer-column"]}>
+        <div className={style["footer-column" + " " + style["contact"]]}>
           <h5 className={style["footer-heading"]}>Contact Info</h5>
           <p className={style["contact-item"]}>Evangadi Networks</p>
           <a
             href="mailto:support@evangadi.com"
-           
             className={style["footer-link"] + " " + style["contact-item"]}
           >
             support@evangadi.com
