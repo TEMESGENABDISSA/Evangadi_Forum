e85f40fee467304f6d40bd11c2264e66efc672b7
rebka <rebkaa22@gmail.com>
2025-10-08 22:45:01 +0300

    signup page added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext, useEffect, useState } from "react";
+import "./App.css";
+import { useNavigate } from "react-router-dom";
+
+import { axiosInstance } from "./utility/axios";
+import AppRouter from "./routes/AppRouter.jsx";
+
+export const UserState = createContext(); // Create a context for the user data
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+
+  const getUserData = async () => {
+    try {
+      const token = localStorage.getItem("Evangadi_Forum"); // Get the token stored during login from local storage
+      if (!token) {
+        navigate("/auth");
+      }
+
+      const userData = await axiosInstance
+        .get("/user/check", { headers: { Authorization: "Bearer " + token } })
+        .then((response) => response.data);
+      console.log(userData);
+      setUser(userData); // Store the user data in state so that it can be accessed by others too
+    } catch (error) {
+      console.log(error);
+      navigate("/auth");
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <UserState.Provider value={{ user, setUser }}>
+      <AppRouter />
+    </UserState.Provider>
+  );
 }
 
-export default App
+export default App;
